6c86470cd8ff80b978e7a534416a8eb6918a397b
mike <mikes.yamauchi@gmail.com>
2025-09-25 14:30:00 -0700

    updated switcher

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,7 +4,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
-
+//comment
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
